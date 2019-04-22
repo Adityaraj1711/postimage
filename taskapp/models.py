7a45b46b51f399ef0bea2467c9f7a6efff471466
Aditya Raj <adityamailbox0@gmail.com>
2019-04-23 00:31:56 +0530

    Update models.py

--- taskapp/models.py
+++ taskapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# for storing images in filefield for MDTouch image upload or document upload.
 class File(models.Model):
     file = models.FileField(blank=False, null=False)
 #    remark = models.CharField(max_length=20)
